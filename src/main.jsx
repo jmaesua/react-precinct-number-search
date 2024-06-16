edb143780d47b3e5a61fa050a674bdb8c6714fc3
Joe Maesua <thywordj11@gmail.com>
2024-06-17 01:48:13 +0800

    base name update

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App base="react-precinct-number-search"/>
   </React.StrictMode>,
   document.getElementById('root')
 )
