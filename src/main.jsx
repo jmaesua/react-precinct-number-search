23d95f6ad935c80cb544a88249399933e8ee9b81
Joe Maesua <thywordj11@gmail.com>
2024-06-17 08:17:53 +0800

    Updating

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App base="react-precinct-number-search"/>
-  </React.StrictMode>,
-  document.getElementById('root')
+    <App />
+  </React.StrictMode>
 )
