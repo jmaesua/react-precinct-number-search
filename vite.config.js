5c461b1acc9beee86c4fa206775e7f1d58cef6c5
Joe Maesua <thywordj11@gmail.com>
2024-06-17 10:31:20 +0800

    update config

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "react-precinct-search-number",
-  // server: {
-  //   historyApiFallback: true,
-  // },
-  // build: {
-  //   outDir: 'dist',
-  // },
-});
+  base: "/react-precinct-search-number/",
+})
