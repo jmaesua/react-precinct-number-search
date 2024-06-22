cd8d0e5448efdd0f09c78a8001abde3ee3a976fd
Joe Maesua <thywordj11@gmail.com>
2024-06-22 18:01:58 +0800

    Update search result lists to display names alphabetically

--- src/components/SearchResultsList.jsx
+++ src/components/SearchResultsList.jsx
@@ -2,9 +2,20 @@ import "./SearchResultsList.css";
 import { SearchResult } from "./SearchResult";
 
 export const SearchResultsList = ({ results, handleResultClick }) => {
+
+  const sortedResults = [...results].sort((a, b) => {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  })
+  
   return (
     <div className="results-list">
-      {results.map((result, index) => (
+      {sortedResults.map((result, index) => (
         <SearchResult key={index} result={result} handleResultClick={handleResultClick} />
       ))}
     </div>
